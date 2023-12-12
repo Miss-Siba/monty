c60e2d559750bfceb1d306fce0cdf9ea3e06a6bc
TshilidziT <Tshilidzibytee@gmail.com>
2023-12-12 12:43:03 -0800

    4

--- add.c
+++ add.c
@@ -21,32 +21,3 @@ void add(struct stack *s)
 
 	push(s, c);
 }
-int main(void)
-{
-	stack *s = (stack *)malloc(sizeof(stack));
-
-	s->current_size = 0;
-	s->head = NULL;
-
-	push(s, 10);
-	push(s, 20);
-	push(s, 30);
-	push(s, 40);
-	push(s, 50);
-
-	printf("Stack before adding:\n");
-	display(s);
-	add(s);
-
-	printf("Stack after adding:\n");
-	display(s);
-
-	assert(pop(s) == 90);
-	assert(pop(s) == 50);
-	assert(pop(s) == 10);
-
-	printf(SUCCESS "tests passed!\n" RESET);
-
-	free(s);
-	return (0);
-}
