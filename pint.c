94034cfe5e9cbdfc7d11441a2f0d866cbc219c89
Miss-Siba <sibaquma@gmail.com>
2023-12-14 13:53:23 -0800

    monty

--- pint.c
+++ pint.c
@@ -9,7 +9,7 @@ void f_pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L<line_number>: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
